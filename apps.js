1e5fcccee3c2875caaaf93afe0420b27b3a2a380
flyingfish16 <phong.duongnam@gmail.com>
2023-07-01 00:02:22 +0900

    push

--- apps.js
+++ apps.js
@@ -1,10 +1,18 @@
-// Disable overscroll on touch devices
-document.addEventListener('DOMContentLoaded', function() {
-    document.body.addEventListener('touchmove', function(e) {
-      // Check if scrolling is possible
-      if (e.currentTarget.scrollHeight > e.currentTarget.clientHeight) {
-        e.preventDefault();
-      }
-    }, { passive: false });
+// // Disable overscroll on touch devices
+// document.addEventListener('DOMContentLoaded', function() {
+//     document.body.addEventListener('touchmove', function(e) {
+//       // Check if scrolling is possible
+//       if (e.currentTarget.scrollHeight > e.currentTarget.clientHeight) {
+//         e.preventDefault();
+//       }
+//     }, { passive: false });
+//   });
+  
+
+window.addEventListener('DOMContentLoaded', function() {
+    var supportsOverflowStyle = 'webkitOverflowScrolling' in document.documentElement.style;
+    if (supportsOverflowStyle) {
+      document.documentElement.style.webkitOverflowScrolling = 'touch';
+    }
   });
   
